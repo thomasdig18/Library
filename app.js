6deea613b95cc0a96331efed8beef3e3a84999ca
Thomas DiGregorio <thomas.digregorio@gmail.com>
2023-02-08 13:34:01 -0500

    Added Html inputs and button that pushes books to myLibrary array

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-let myLibrary = [];
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return title + ' by ' + author + ', ' + pages + ' pages ' + read
-    }
-}
-
-function addBookToLibrary() {
+  }
   
-}
+  const myLibrary = [];
+  
+  const form = document.querySelector("#book-form");
+  const addButton = document.querySelector("#add-button");
+  
+  addButton.addEventListener("click", function() {
+    const titleInput = document.querySelector("#title-input");
+    const authorInput = document.querySelector("#author-input");
+    const pagesInput = document.querySelector("#pages-input");
+    const readInput = document.querySelector("#read-input");
+    
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const read = readInput.checked;
+    
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.checked = false;
+  });
