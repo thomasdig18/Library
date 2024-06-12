7aa3dfcca836d5d6d77d646136dbcdf86060f981
Thomas DiGregorio <thomas.digregorio@gmail.com>
2024-06-12 10:14:45 -0400

    style changes and refactor of app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-?????
+const myLibrary = [];
+
+function Book(title, author, pages, isread) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isread = isread; 
+
+}
+
+
+function addBookToLibrary(title, author, pages, isread) {
+  let newBook = new Book(title, author, pages, isread);
+  myLibrary.push(newBook);
+}
+
